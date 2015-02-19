6d83b939c3ce84bf7f63f8d92fed0223da9a8a38
Tonys Work Laptop <THansenite@gmail.com>
2015-02-19 14:50:26 -0600

    added teams include file to project so it is easier to modify team hyperlinks

--- test/redalert.php
+++ test/redalert.php
@@ -24,48 +24,11 @@
 		<div id="bg">
 			<div id="outer">
 				<div id="header">
-				<?php include ("common/header.php");?>
+					<?php include ("common/header.php");?>
 				</div>
 				<div id="main">
 					<div id="sidebar1">
-						<h3>
-							Teams
-						</h3>
-						<ul class="linkedList">
-							<li class="first">
-								<a href="#">Alien Hockey</a>
-							</li>
-							<li>
-								<a href="#">Flying Moose Knuckles</a>
-							</li>
-							<li>
-								<a href="#">Cup O Kryptonite</a>
-							</li>
-							<li>
-								<a href="#">Forklifts of DM/Kyle's Bikes</a>
-							</li>
-							<li>
-								<a href="#">Ichi Bikes</a>
-							</li>
-							<li>
-								<a href="#">Red Alert</a>
-							</li>
-							<li>
-								<a href="#">Rink Rats</a>
-							</li>
-							<li class="last">
-								<a href="#">Victors</a>
-							</li>
-						</ul>
-						<h3>
-							Aliquam etiam adipiscing
-						</h3>
-						<p>
-							Facilisis non dictum ultricies. Purus et enim mi iaculis non magna vitae ipsum. Ultricies mattis risus sed
-							sed mattis. Turpis placerat lectus quis consequat tincidunt felis. Fermentum et purus mollis posuere
-							aliquam amet commodo sed blandit curae lorem ipsum nascetur congue in sed diam nascetur arcu. Arcu sed
-							consectetur fringilla amet tellus montes.
-						</p>
+						<?php include ("common/teams.php");?>
 					</div>
 					<div id="sidebar2">
 						<h3>

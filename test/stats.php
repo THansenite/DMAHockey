bd5a1b2107a4547f16b89767c09c6e0eb6ca89f5
unknown <hansenan@US-DDTGYZ1.phibred.com>
2015-02-23 17:03:16 -0600

    Worked on mock-up of played and upcoming games

--- test/stats.php
+++ test/stats.php
@@ -31,31 +31,64 @@
 						<?php include ("common/teams.php");?>
 					</div>
 					<div id="sidebar2">
-						<h3>
-							Recent Scores
-						</h3>
-						<ul class="linkedList">
-							<li class="first">
-								<a href="#">FoDM/KB - 5<br />Red Alert - 8</a>
-							</li>
-							<li>
-								<a href="#">Ichi - 3<br />Alien - 5</a>
-							</li>
-							<li>
-								<a href="#">Kryptonite - 3<br />Rink Rats - 2</a>
-							</li>
-							<li class="last">
-								<a href="#">Victors - 5<br />Flying Moose - 3</a>
-							</li>
-						</ul>
-						<h3>
-							Etiam hendrerit
-						</h3>
-						<img class="top" src="images/pic2.jpg" width="252" height="80" alt="" />
-						<p>
-							Turpis placerat lectus quis consequat tincidunt felis. Fermentum et purus mollis posuere aliquam 
-							commodo sed blandit curae lorem ipsum.
-						</p>
+						<h2>
+							Recent Games
+						</h2>
+								<table id="recent_games">
+								<tr><td colspan=2><hr /></td></tr>
+									<tr><td>2/22</td><td>6:45PM</tr>
+									<tr class="loser"><td>Victors</td><td>2</td></tr>
+									<tr class="winner"><td>Rink Rats</td><td>4</td></tr>
+									<tr><td colspan = 2><em>FINAL<em></td></tr>
+								<tr><td colspan=2><hr /></td></tr>
+									<tr><td>2/22</td><td>8:05PM</tr>
+									<tr class="loser"><td>Flying Moose</td><td>5</td></tr>
+									<tr class="winner"><td>Alien</td><td>6</td></tr>
+									<tr><td colspan = 2><em>FINAL<em></td></tr>
+								<tr><td colspan=2><hr /></td></tr>
+									<tr><td>2/22</td><td>9:25PM</tr>
+									<tr class="winner"><td>Kryptonite</td><td>4</td></tr>
+									<tr class="loser"><td>FoDM/KB</td><td>3</td></tr>
+									<tr><td colspan = 2><em>FINAL<em></td></tr>
+								<tr><td colspan=2><hr /></td></tr>
+									<tr><td>2/22</td><td>10:45PM</tr>
+									<tr class="loser"><td>Red Alert</td><td>7</td></tr>
+									<tr class="winner"><td>Ichi Bike</td><td>7</td></tr>
+									<tr><td colspan = 2><em>Shootout<em></td></tr>
+								<tr><td colspan=2><hr /></td></tr>
+								</table>
+
+								<br />
+						<h2>
+							Upcoming Games
+						</h2>
+						
+								<table id="upcoming_games">
+								<tr><td><hr /></td></tr>
+									<tr><td>2/22</td></tr>
+									<tr class="team"><td>Ichi Bike</td></tr>
+									<tr class="team"><td>Alien</td></tr>
+									<tr><td><em>6:45PM<em></td>
+								</tr>
+								<tr><td><hr /></td></tr>
+									<tr><td>2/22</td></tr>
+									<tr class="team"><td>Kryptonite</td></tr>
+									<tr class="team"><td>Rink Rats</td></tr>
+									<tr><td><em>8:05PM<em></td>
+								</tr>
+								<tr><td><hr /></td></tr>
+									<tr><td>2/22</td></tr>
+									<tr class="team"><td>Flying Moose</td></tr>
+									<tr class="team"><td>Victors</td></tr>
+									<tr><td><em>9:25PM<em></td>
+								</tr>
+								<tr><td><hr /></td></tr>
+									<tr><td>2/22</td></tr>
+									<tr class="team"><td>Red Alert</td></tr>
+									<tr class="team"><td>FoDM/KB</td></tr>
+									<tr><td><em>10:45PM<em></td>
+								</tr>
+								</table>
 					</div>
 					<div id="content">
 						<div id="box1">

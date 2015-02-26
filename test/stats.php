b71e12df8db8f66fc9d598b0f50265e996449535
unknown <hansenan@US-DDTGYZ1.phibred.com>
2015-02-26 12:33:39 -0600

    Pulled recent scores out of stats page and made its own include; currently static and needs to be made dynamic

--- test/stats.php
+++ test/stats.php
@@ -31,69 +31,12 @@
 						<?php include ("common/teams.php");?>
 					</div>
 					<div id="sidebar2">
-						<h2>
-							Recent Games
-						</h2>
-								<table id="recent_games">
-								<tr><td colspan=2><hr /></td></tr>
-									<tr><td>2/22</td><td>6:45PM</tr>
-									<tr class="loser"><td>Victors</td><td>2</td></tr>
-									<tr class="winner"><td>Rink Rats</td><td>4</td></tr>
-									<tr><td colspan = 2><em>FINAL<em></td></tr>
-								<tr><td colspan=2><hr /></td></tr>
-									<tr><td>2/22</td><td>8:05PM</tr>
-									<tr class="loser"><td>Flying Moose</td><td>5</td></tr>
-									<tr class="winner"><td>Alien</td><td>6</td></tr>
-									<tr><td colspan = 2><em>FINAL<em></td></tr>
-								<tr><td colspan=2><hr /></td></tr>
-									<tr><td>2/22</td><td>9:25PM</tr>
-									<tr class="winner"><td>Kryptonite</td><td>4</td></tr>
-									<tr class="loser"><td>FoDM/KB</td><td>3</td></tr>
-									<tr><td colspan = 2><em>FINAL<em></td></tr>
-								<tr><td colspan=2><hr /></td></tr>
-									<tr><td>2/22</td><td>10:45PM</tr>
-									<tr class="loser"><td>Red Alert</td><td>7</td></tr>
-									<tr class="winner"><td>Ichi Bike</td><td>7</td></tr>
-									<tr><td colspan = 2><em>Shootout<em></td></tr>
-								<tr><td colspan=2><hr /></td></tr>
-								</table>
-
-								<br />
-						<h2>
-							Upcoming Games
-						</h2>
-						
-								<table id="upcoming_games">
-								<tr><td><hr /></td></tr>
-									<tr><td>2/22</td></tr>
-									<tr class="team"><td>Ichi Bike</td></tr>
-									<tr class="team"><td>Alien</td></tr>
-									<tr><td><em>6:45PM<em></td>
-								</tr>
-								<tr><td><hr /></td></tr>
-									<tr><td>2/22</td></tr>
-									<tr class="team"><td>Kryptonite</td></tr>
-									<tr class="team"><td>Rink Rats</td></tr>
-									<tr><td><em>8:05PM<em></td>
-								</tr>
-								<tr><td><hr /></td></tr>
-									<tr><td>2/22</td></tr>
-									<tr class="team"><td>Flying Moose</td></tr>
-									<tr class="team"><td>Victors</td></tr>
-									<tr><td><em>9:25PM<em></td>
-								</tr>
-								<tr><td><hr /></td></tr>
-									<tr><td>2/22</td></tr>
-									<tr class="team"><td>Red Alert</td></tr>
-									<tr class="team"><td>FoDM/KB</td></tr>
-									<tr><td><em>10:45PM<em></td>
-								</tr>
-								</table>
+						<?php include ("common/recent_scores.php");?>
 					</div>
 					<div id="content">
 						<div id="box1">
 
-<h1>Top Scorers</h1>
+<h1>Points Leaders</h1>
 <hr />
 <br />
 <table id="scorers" align="center" width="90%">

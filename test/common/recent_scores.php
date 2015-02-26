32747047d979a490917776fa9d4f4550e940f2eb
unknown <hansenan@US-DDTGYZ1.phibred.com>
2015-02-26 14:53:16 -0600

    Worked on recent_scores formatting; still static data, but looks like I want it to now

--- test/common/recent_scores.php
+++ test/common/recent_scores.php
@@ -4,27 +4,27 @@ echo '
 	Recent Games
 </h2>
 		<table id="recent_games">
-		<tr><td colspan=2><hr /></td></tr>
+		<tr><td class="divider" colspan=2><hr /></td></tr>
 			<tr><td>2/22</td><td>6:45PM</tr>
 			<tr class="loser"><td>Victors</td><td>2</td></tr>
 			<tr class="winner"><td>Rink Rats</td><td>4</td></tr>
 			<tr><td colspan = 2><em>FINAL<em></td></tr>
-		<tr><td colspan=2><hr /></td></tr>
+		<tr><td class="divider" colspan=2><hr /></td></tr>
 			<tr><td>2/22</td><td>8:05PM</tr>
 			<tr class="loser"><td>Flying Moose</td><td>5</td></tr>
 			<tr class="winner"><td>Alien</td><td>6</td></tr>
 			<tr><td colspan = 2><em>FINAL<em></td></tr>
-		<tr><td colspan=2><hr /></td></tr>
+		<tr><td class="divider" colspan=2><hr /></td></tr>
 			<tr><td>2/22</td><td>9:25PM</tr>
 			<tr class="winner"><td>Kryptonite</td><td>4</td></tr>
 			<tr class="loser"><td>FoDM/KB</td><td>3</td></tr>
 			<tr><td colspan = 2><em>FINAL<em></td></tr>
-		<tr><td colspan=2><hr /></td></tr>
+		<tr><td class="divider" colspan=2><hr /></td></tr>
 			<tr><td>2/22</td><td>10:45PM</tr>
 			<tr class="loser"><td>Red Alert</td><td>7</td></tr>
 			<tr class="winner"><td>Ichi Bike</td><td>7</td></tr>
 			<tr><td colspan = 2><em>SHOOTOUT<em></td></tr>
-		<tr><td colspan=2><hr /></td></tr>
+		<tr><td class="divider" colspan=2><hr /></td></tr>
 		</table>
 
 		<br />
@@ -33,30 +33,27 @@ echo '
 </h2>
 
 		<table id="upcoming_games">
-		<tr><td><hr /></td></tr>
-			<tr><td>2/22</td></tr>
-			<tr class="team"><td>Ichi Bike</td></tr>
-			<tr class="team"><td>Alien</td></tr>
-			<tr><td><em>6:45PM<em></td>
+		<tr><td class="divider" colspan=2><hr /></td></tr>
+			<tr><td class="team">Ichi Bike</td><td><em>2/22<em></td></tr>
+			<tr><td class="team">Alien</td><td><em>6:45PM<em></td>
 		</tr>
-		<tr><td><hr /></td></tr>
-			<tr><td>2/22</td></tr>
-			<tr class="team"><td>Kryptonite</td></tr>
-			<tr class="team"><td>Rink Rats</td></tr>
-			<tr><td><em>8:05PM<em></td>
+		<table id="upcoming_games">
+		<tr><td class="divider" colspan=2><hr /></td></tr>
+			<tr><td class="team">Kryptonite</td><td><em>2/22<em></td></tr>
+			<tr><td class="team">Rink Rats</td><td><em>8:05PM<em></td>
 		</tr>
-		<tr><td><hr /></td></tr>
-			<tr><td>2/22</td></tr>
-			<tr class="team"><td>Flying Moose</td></tr>
-			<tr class="team"><td>Victors</td></tr>
-			<tr><td><em>9:25PM<em></td>
+		<table id="upcoming_games">
+		<tr><td class="divider" colspan=2><hr /></td></tr>
+			<tr><td class="team">Flying Moose</td><td><em>2/22<em></td></tr>
+			<tr><td class="team">Victors</td><td><em>9:25PM<em></td>
 		</tr>
-		<tr><td><hr /></td></tr>
-			<tr><td>2/22</td></tr>
-			<tr class="team"><td>Red Alert</td></tr>
-			<tr class="team"><td>FoDM/KB</td></tr>
-			<tr><td><em>10:45PM<em></td>
+		<table id="upcoming_games">
+		<tr><td class="divider" colspan=2><hr /></td></tr>
+			<tr><td class="team">Red Alert</td><td><em>2/22<em></td></tr>
+			<tr><td class="team">FoDM/KB</td><td><em>10:45PM<em></td>
+			<tr><td class="divider" colspan=2><hr /></td></tr>
 		</tr>
+
 		</table>
 ';
 ?>

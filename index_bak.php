152241b42cd74dc2641ab057fb786901a3333334
unknown <hansenan@US-DDTGYZ1.phibred.com>
2015-02-27 10:45:04 -0600

    deleted old files from previous website

--- index_bak.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<head>
-	<title>DMAHockey.com - Home</title>
-	<link href="common/styles.css" rel="stylesheet">
-</head>
-<body>
-<h1>DMAHockey.com</h1>
-<p>Welcome to the homepage of the Alien/Bucs C League</p>
-</body>
